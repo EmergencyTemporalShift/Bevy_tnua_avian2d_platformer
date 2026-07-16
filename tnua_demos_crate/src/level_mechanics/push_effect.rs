706b9fabb39975ccbd486864a56a5cd96c2416ca
EmergencyTemporalShift <jrisenhour@gmail.com>
2026-07-16 09:54:52 -0400

    Initial build of the bevy-tnua avian2d platform demo ripped out into its own project.

---
+++ tnua_demos_crate/src/level_mechanics/push_effect.rs
@@ -0,0 +1,38 @@
+use bevy::prelude::*;
+use bevy_tnua::{
+    builtins::TnuaBuiltinKnockback,
+    math::{AsF32, Vector3},
+    prelude::TnuaController,
+};
+
+use crate::character_control_systems::platformer_control_scheme::DemoControlScheme;
+
+pub struct PushEffectPlugin;
+
+impl Plugin for PushEffectPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, apply_push_effect);
+    }
+}
+
+#[derive(Component)]
+pub enum PushEffect {
+    Impulse(Vector3),
+}
+
+fn apply_push_effect(
+    mut query: Query<(Entity, &PushEffect, &mut TnuaController<DemoControlScheme>)>,
+    mut commands: Commands,
+) {
+    for (entity, push_effect, mut controller) in query.iter_mut() {
+        match push_effect {
+            PushEffect::Impulse(impulse) => {
+                controller.action_interrupt(DemoControlScheme::Knockback(TnuaBuiltinKnockback {
+                    shove: *impulse,
+                    force_forward: Dir3::new(-impulse.reject_from(Vector3::Y).f32()).ok(),
+                }));
+                commands.entity(entity).remove::<PushEffect>();
+            }
+        }
+    }
+}
